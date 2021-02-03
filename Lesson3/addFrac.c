00a84b03eb18f4650ad530fb151bdc66dfa65764
Cristian Lepore <cristianlepore24@gmail.com>
2021-02-03 17:23:45 +0000

    .

---
+++ Lesson3/addFrac.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2, den1, den2;
+
+    printf("Enter first fraction: ");
+    scanf("%d/%d", &num1, &den1);
+
+    printf("Enter second fraction: ");
+    scanf("%d/%d", &num2, &den2);
+
+    int num = (num1 * den2) + (num2 * den1);
+    int denom = den1 * den2;
+
+    printf("The sum is %d/%d", num, denom);   
+
+    return 0;
+}
